174af1f1e31791596610f9e974b744f3adc85abc
andressaribeiroo <andressaclecia.ribeiro@gmail.com>
2024-03-17 17:43:50 -0300

    feat(api): add user repository

--- backend/src/main/java/com/uol/backend/Services/exceptions/DataIntegratyViolationException.java
+++ backend/src/main/java/com/uol/backend/Services/exceptions/DataIntegratyViolationException.java
@@ -1,6 +1,5 @@
 package com.uol.backend.Services.exceptions;
 
-
 public class DataIntegratyViolationException extends RuntimeException{
 
     public DataIntegratyViolationException(String message) {

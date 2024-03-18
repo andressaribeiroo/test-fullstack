dffae099c66d0aff6c6b8028db6045fd638d13ab
andressaribeiroo <andressaclecia.ribeiro@gmail.com>
2024-03-18 00:44:34 -0300

    feat(frontend): add style nav

--- frontend/frontend/src/app/app.component.ts
+++ frontend/frontend/src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, NavComponent],
   templateUrl: `app.component.html`,
   styles: [],
 })
